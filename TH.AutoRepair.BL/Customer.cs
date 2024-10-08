d5a20285366a6c1787d113967a76aadd213142fe
700211854 <700211854@APPC132B17>
2024-10-08 18:07:17 -0500

    Add project files.

---
+++ TH.AutoRepair.BL/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace TH.AutoRepair.BL
+{
+    public class Customer
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+
+        public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+
+    }
+}
